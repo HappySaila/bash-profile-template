7c65f7a97d00a3cc91bf1510f1586621a24de677
HappySaila <grant.wilson@sovtech.com>
2024-07-28 21:37:32 +0200

    feat

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,9 @@
 # Write .bashrc linking commands
 echo 'export DirectoryRepos=~/repos
-source $DirectoryRepos/bash-profile-template/.bash_profile
-source $DirectoryRepos/bash-profile-template/.bash_profile' > ~/.bashrc
+source $DirectoryRepos/bash-profile/.bash_profile
+source $DirectoryRepos/bash-profile/.bash_profile' > ~/.bashrc
 echo 'export DirectoryRepos=~/repos
-source $DirectoryRepos/bash-profile-template/.bash_profile
-source $DirectoryRepos/bash-profile-template/.bash_profile' > ~/.bash_profile
+source $DirectoryRepos/bash-profile/.bash_profile
+source $DirectoryRepos/bash-profile/.bash_profile' > ~/.bash_profile
 source ~/.bashrc
 source ~/.bash_profile
